5ebc997a9c776eb2f7f4c31ef99e816f93f1ba84
Mateus-Pessini <mateus.scherer@edu.unipar.br>
2023-11-08 22:13:14 -0300

    .

--- Projeto_Integrador-main/DentiCare/src/main/java/br/unipar/dentiCare/controllers/PessoaController.java
+++ Projeto_Integrador-main/DentiCare/src/main/java/br/unipar/dentiCare/controllers/PessoaController.java
@@ -11,42 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "/pessoa")
-@Api(tags = "Pessoas", description = "Rota relacionada com Pessoa")
 public class PessoaController {
 
-    @Autowired
-    PessoaService pessoaService;
-
-    @PostMapping
-    @ApiOperation(value = "Insere um Pessoa")
-    public Pessoa insert(@Valid @RequestBody PessoaDTO pessoaDTO) throws Exception{
-        return pessoaService.insert(pessoaDTO);
-    }
-
-    @PutMapping
-    @ApiOperation(value = "Edita um Pessoa")
-    public Pessoa edit(@Valid @RequestBody Pessoa pessoa) throws Exception{
-        return pessoaService.edit(pessoa);
-    }
-
-    @DeleteMapping(path = "/{id}")
-    @ApiOperation(value = "Remove um Pessoa")
-    public void remove(@PathVariable Long id) throws Exception{
-        pessoaService.remove(id);
-    }
-
-    @GetMapping
-    @ApiOperation("Retorna uma lista de Pessoas")
-    public List<Pessoa> findAll(){
-        return pessoaService.findAll();
-    }
-
-    @GetMapping(path = "/{id}")
-    @ApiOperation(value = "Busca um Pessoa pelo id")
-    public Pessoa findById(@PathVariable Long id) throws Exception{
-        return pessoaService.findById(id);
-    }
 
 }
